a4a9cca331b669ca3127928dc9934403b414d877
Minna Yu <minyu@chapman.edu>
2020-11-11 10:55:24 -0800

    updates regex

--- username.sh
+++ username.sh
@@ -3,7 +3,7 @@
 # Minna Yu 
 echo "Enter a Username: "
 read USER 
-while echo "$USER" | egrep -v "^[a-z][0-9][-]$" > /dev/null 2>&1
+while echo "$USER" | egrep -v "^[a-z][a-z0-9_]{3,12}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username - lower case letters, digits, and underscores only!"
 	echo "Enter a username: "
