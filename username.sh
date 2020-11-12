2bfe8876c829262d1b38967875a9a36a4e091dbb
Minna Yu <minyu@chapman.edu>
2020-11-11 19:58:57 -0800

    adds username.sh

---
+++ username.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# username.sh
+# Minna Yu
+echo "Enter a username: "
+read USER
+while echo "$USER" | egrep -v "^[a-z][a-z0-9_]{3,11}$" > /dev/null 2>&1
+do
+	echo "You must enter a valid username!"
+	echo "Must start with a letter, contain numbers, and be at least 3 characters but no more than 12."
+	echo "Enter a username: "
+	read USER
+done
+echo "Thank you"
